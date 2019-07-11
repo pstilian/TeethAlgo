635ce2ecc6057a77687da375d104b4e7b806d3e2
jteav <jmteav@gmail.com>
2019-07-11 16:19:21 -0400

    table

--- teeth.cpp
+++ teeth.cpp
@@ -131,7 +131,9 @@ int main(){
     for(int i = 0 ; i < m; i++){
         for(int j = 0; j < n; j++){
             LT[i][j] = topTeeth.at(j) + bottomTeeth.at(i);
+			cout << LT[i][j] << '\t';
         }
+		cout << endl;
     }
 
 	// Find minVal base case
